5e1db5b23ac88b83a0ac406e98b0dbdea7cf02dc
Tyler Wolf <tyler@twolf.io>
2023-08-07 14:43:00 -0400

    emacs rehaul

--- home/tyler/.emacs.d/init.el
+++ home/tyler/.emacs.d/init.el
@@ -4,7 +4,8 @@
 ;; close pairs
 (electric-pair-mode)
 
-(tool-bar-mode)
+;; remove tool bar
+(tool-bar-mode -1)
 
 ;; change backup directory
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
@@ -121,121 +122,23 @@
 ;; pdf-tools
 (pdf-loader-install)
 
-;; mastodon
+;; ement.el
+(defun my/ement-connect ()
+  (interactive)
+  (ement-connect :uri-prefix "http://localhost:8009"
+		 :user-id "@tylerwolf:matrix.org"))
+
+;; mastodon.el
 (setq mastodon-instance-url "https://freeradical.zone"
       mastodon-active-user "tylerwolf")
 
-;; mail
-(setq user-mail-address "tylerwolf@posteo.net")
-(setq user-full-name "Tyler Wolf")
+;; telega.el
+(define-key global-map (kbd "C-c t") telega-prefix-map)
+(setq telega-translate-to-language-by-default "en")
 
-(setq gnus-select-method
-      '(nnimap "posteo.de"))
-
-(setq send-mail-function 'smtpmail-send-it
-      smtpmail-smtp-server "posteo.de"
-      smtpmail-smtp-service 465
-      smtpmail-stream-type 'ssl
-      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
-
-;; gnus-group-mode
-(eval-after-load 'gnus-group
-  '(progn
-     (defhydra hydra-gnus-group (:color blue)
-       "
-[_A_] Remote groups (A A) [_g_] Refresh
-[_L_] Local groups        [_\\^_] List servers
-[_c_] Mark all read       [_m_] Compose new mail
-[_G_] Search mails (G G) [_#_] Mark mail
-"
-       ("A" gnus-group-list-active)
-       ("L" gnus-group-list-all-groups)
-       ("c" gnus-topic-catchup-articles)
-       ("G" dianyou-group-make-nnir-group)
-       ("g" gnus-group-get-new-news)
-       ("^" gnus-group-enter-server-mode)
-       ("m" gnus-group-new-mail)
-       ("#" gnus-topic-mark-topic)
-       ("q" nil))
-     ;; y is not used by default
-     (define-key gnus-group-mode-map "y" 'hydra-gnus-group/body)))
-
-;; gnus-summary-mode
-(eval-after-load 'gnus-sum
-  '(progn
-     (defhydra hydra-gnus-summary (:color blue)
-       "
-[_s_] Show thread   [_F_] Forward (C-c C-f)
-[_h_] Hide thread   [_e_] Resend (S D e)
-[_n_] Refresh (/ N) [_r_] Reply
-[_!_] Mail -> disk  [_R_] Reply with original
-[_d_] Disk -> mail  [_w_] Reply all (S w)
-[_c_] Read all      [_W_] Reply all with original (S W)
-[_#_] Mark          [_G_] Search mails
-"
-       ("s" gnus-summary-show-thread)
-       ("h" gnus-summary-hide-thread)
-       ("n" gnus-summary-insert-new-articles)
-       ("F" gnus-summary-mail-forward)
-       ("!" gnus-summary-tick-article-forward)
-       ("d" gnus-summary-put-mark-as-read-next)
-       ("c" gnus-summary-catchup-and-exit)
-       ("e" gnus-summary-resend-message-edit)
-       ("R" gnus-summary-reply-with-original)
-       ("r" gnus-summary-reply)
-       ("W" gnus-summary-wide-reply-with-original)
-       ("w" gnus-summary-wide-reply)
-       ("#" gnus-topic-mark-topic)
-       ("G" dianyou-group-make-nnir-group)
-       ("q" nil))
-     ;; y is not used by default
-     (define-key gnus-summary-mode-map "y" 'hydra-gnus-summary/body)))
-
-;; gnus-article-mode
-(eval-after-load 'gnus-art
-  '(progn
-     (defhydra hydra-gnus-article (:color blue)
-       "
-[_o_] Save attachment        [_F_] Forward
-[_v_] Play video/audio       [_r_] Reply
-[_d_] CLI to download stream [_R_] Reply with original
-[_b_] Open external browser  [_w_] Reply all (S w)
-[_f_] Click link/button      [_W_] Reply all with original (S W)
-[_g_] Focus link/button
-"
-       ("F" gnus-summary-mail-forward)
-       ("r" gnus-article-reply)
-       ("R" gnus-article-reply-with-original)
-       ("w" gnus-article-wide-reply)
-       ("W" gnus-article-wide-reply-with-original)
-       ("o" gnus-mime-save-part)
-       ("v" my-w3m-open-with-mplayer)
-       ("d" my-w3m-download-rss-stream)
-       ("b" my-w3m-open-link-or-image-or-url)
-       ("f" w3m-lnum-follow)
-       ("g" w3m-lnum-goto)
-       ("q" nil))
-     ;; y is not used by default
-     (define-key gnus-article-mode-map "y" 'hydra-gnus-article/body)))
-
-;; message-mode
-(eval-after-load 'message
-  '(progn
-     (defhydra hydra-message (:color blue)
-  "
-[_c_] Complete mail address
-[_a_] Attach file
-[_s_] Send mail (C-c C-c)
-"
-       ("c" counsel-bbdb-complete-mail)
-       ("a" mml-attach-file)
-       ("s" message-send-and-exit)
-       ("i" dianyou-insert-email-address-from-received-mails)
-       ("q" nil))))
-
-(defun message-mode-hook-hydra-setup ()
-  (local-set-key (kbd "C-c C-y") 'hydra-message/body))
-(add-hook 'message-mode-hook 'message-mode-hook-hydra-setup)
+;; initialize path from shell
+(when (daemonp)
+  (exec-path-from-shell-initialize))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
---
+++ home/tyler/.gnus.el
@@ -0,0 +1,48 @@
+;; Personal Information
+(setq user-full-name "Tyler Wolf"
+      user-mail-address "tylerwolf@posteo.net")
+
+
+;; IMAP
+(setq gnus-select-method '(nnimap "posteo.de"))
+
+;; SMTP
+(setq send-mail-function 'smtpmail-send-it
+      smtpmail-smtp-server "posteo.de"
+      smtpmail-smtp-service 465
+      smtpmail-stream-type 'ssl)
+
+;; Sent folder
+(setq gnus-message-archive-method '(nnimap "posteo.de")
+       gnus-message-archive-group "Sent")
+
+;; auto-complete emacs address using bbdb command, optional
+(add-hook 'message-mode-hook
+          '(lambda ()
+             (flyspell-mode t)
+             (local-set-key (kbd "TAB") 'bbdb-complete-name)))
+;; }}
+
+;; ask encryption password once
+(setq epa-file-cache-passphrase-for-symmetric-encryption t)
+
+(setq gnus-thread-sort-functions
+      '(gnus-thread-sort-by-most-recent-date
+        (not gnus-thread-sort-by-number)))
+
+; NO 'passive
+(setq gnus-use-cache t)
+
+;; {{ press "o" to view all groups
+(defun my-gnus-group-list-subscribed-groups ()
+  "List all subscribed groups with or without un-read messages"
+  (interactive)
+  (gnus-group-list-all-groups 5))
+
+(define-key gnus-group-mode-map
+  ;; list all the subscribed groups even they contain zero un-read messages
+  (kbd "o") 'my-gnus-group-list-subscribed-groups)
+;; }}
+
+;; http://www.gnu.org/software/emacs/manual/html_node/gnus/_005b9_002e2_005d.html
+(setq gnus-use-correct-string-widths nil)
